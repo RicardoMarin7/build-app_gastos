28e8a1817613cf64ae87b603922abe7a82269909
Ricardo Marin <Ricardo_marin97@hotmail.com>
2020-07-16 19:24:51 -0500

    Versiona 1.0 App

---
+++ precache-manifest.4429d2e864f7ee29e23bb6675283c0a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6066c58fae3701e7d4b5a46e21ca5bc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88ed76a6cc8f6f04633e",
+    "url": "/static/css/main.6387e0e2.chunk.css"
+  },
+  {
+    "revision": "3f61bc54c4937e5ec0bd",
+    "url": "/static/js/2.02eb72df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.02eb72df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88ed76a6cc8f6f04633e",
+    "url": "/static/js/main.bef502f9.chunk.js"
+  },
+  {
+    "revision": "224d8ec47b0b615b11f5",
+    "url": "/static/js/runtime-main.b0160080.js"
+  }
+]);
